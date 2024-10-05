b5a2ed5d895aca3a47a77e8d12ca8047f8a26e8f
John Rizzo <johnrizzo1@gmail.com>
2024-10-05 10:27:32 -0400

    Chapter 4 Complete

--- devenv.nix
+++ devenv.nix
@@ -1,37 +1,42 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.git ];
-  # languages.rust.enable = true;
+  # packages = [ pkgs.git pkgs.pnpm ];
+
   languages.javascript.enable = true;
   languages.javascript.npm.enable = true;
   languages.javascript.pnpm.enable = true;
   languages.typescript.enable = true;
-  
-  dotenv.enable = true;
-  
-  services.postgres.enable = true;
-  services.postgres.listen_addresses = "0.0.0.0";
-  services.postgres.port = 5432;
-  # services.postgres.initialDatabases = [
-  #   { name = "verceldb"; }
-  # ];
 
-  services.postgres.settings = {
-    log_connections = true;
-    log_statement = "all";
-    logging_collector = true;
-    log_disconnections = true;
-    log_destination = lib.mkForce "syslog";
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.quiet = true;
+    venv.requirements = ./requirements.txt;
   };
 
-  services.postgres.initialDatabases = [
-    {
-      name = "verceldb";
-      pass = "verceluser";
-      user = "verceluser";
-    }
-  ];
+  dotenv.enable = true;
+  
+  services.postgres = {
+    enable = true;
+    listen_addresses = "0.0.0.0";
+    port = 5432;
+    settings = {
+      log_connections = true;
+      log_statement = "all";
+      logging_collector = true;
+      log_disconnections = true;
+      log_destination = lib.mkForce "syslog";
+    };
+
+    initialDatabases = [
+      {
+        name = "verceldb";
+        pass = "verceluser";
+        user = "verceluser";
+      }
+    ];
+  };
 
   enterShell = ''
     git --version
